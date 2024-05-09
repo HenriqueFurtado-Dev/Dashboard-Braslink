1a1d7fa0b4a821d034efd57cfe1c27d3a285afed
Henrique Furtado <cfurtado@intra.braslink.com.br>
2024-05-09 11:49:19 -0300

    :sparkles: Upload file and show first lines

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-import streamlit as st
 import pandas as pd
-import numpy as np
+import streamlit as st
 
-st.title('Braslink - Análise e Relatorios Global')
+st.title("Braslink - Análises e Relatórios")
+
+uploaded_file = st.file_uploader("Selecione um arquivo CSV no padrão export da Global")
+
+if uploaded_file is not None:
+    try:
+        df = pd.read_csv(uploaded_file, sep=';', encoding='latin1')
+        st.write(df.head())
+    except Exception as e:
+        st.error(f"Erro ao ler o arquivo: {e}")
