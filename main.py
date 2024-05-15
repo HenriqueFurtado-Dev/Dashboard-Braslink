02663d991a14165dbc44d20f98bf5df52890a931
Henrique Furtado <cfurtado@intra.braslink.com.br>
2024-05-15 13:48:42 -0300

    :recycle: refactor: Define main and functions in project

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import pandas as pd
 import streamlit as st
 
-st.title("Braslink - Análises e Relatórios")
+def main():
+    st.title("Braslink - Análises e Relatórios")
+    uploaded_file = st.file_uploader("Selecione um arquivo CSV no padrão export da Global")
+    if uploaded_file is not None:
+        try:
+            df = pd.read_csv(uploaded_file, sep=';', encoding='latin1')
+            first_analyse(df)
+        except Exception as e:
+            st.error(f"Erro ao ler o arquivo: {e}")
 
-uploaded_file = st.file_uploader("Selecione um arquivo CSV no padrão export da Global")
+def first_analyse(df):
+    st.write(df.head())
 
-if uploaded_file is not None:
-    try:
-        df = pd.read_csv(uploaded_file, sep=';', encoding='latin1')
-        st.write(df.head())
-    except Exception as e:
-        st.error(f"Erro ao ler o arquivo: {e}")
+
+if __name__ == '__main__':
+    main()
